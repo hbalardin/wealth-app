fa6dc72a7519a6f7f3f167a1349083cf0cfbd953
Henrique Balardin <balardinh@gmail.com>
2022-02-06 13:17:49 -0300

    feat: create page layout component

---
+++ src/components/layout/PageLayout/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 100vh;
+  padding: 32px 24px;
+
+  > main {
+    height: 100%;
+    width: 100%;
+    max-width: 980px;
+    margin: 0 auto;
+  }
+`;
